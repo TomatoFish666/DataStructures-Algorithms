0918997ed370f4cef840458997e6c1374683c614
TomatoFishs <tomatofishs@gmail.com>
2016-03-06 10:16:34 +0800

    卷土重来

---
+++ HDU/HDU_1000.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(scanf("%d%d",&a,&b)==2)
+        printf("%d\n",a+b);
+    return 0;
+}
